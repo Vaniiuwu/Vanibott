7e1dc1cd117b10ddc1e2ad8198fc078f101feef6
Vaniiuwu <87722488+Vaniiuwu@users.noreply.github.com>
2023-11-15 16:04:37 +0100

    Add files via upload

---
+++ Vanibot/commands/Invite.js
@@ -0,0 +1,7 @@
+module.exports = {
+    name: 'Invite',
+    description: "Sends invites to our server!",
+    execute(message, args){
+        message.channel.send('https://www.discord.gg/HornyEmpire (Where my mom lives)');
+    }
+}
---
+++ Vanibot/commands/partners.js
@@ -0,0 +1,7 @@
+module.exports = {
+    name: 'partners',
+    description: "Sends invites to our partners!",
+    execute(message, args){
+        message.channel.send('https://discord.gg/KCx6c5qrDM');
+    }
+}
