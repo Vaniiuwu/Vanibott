08771b7bee6daa52efdc42c510bd10e7764baa97
Vaniiuwu <87722488+Vaniiuwu@users.noreply.github.com>
2023-11-15 16:10:43 +0100

    Delete Vanibot/commands directory

--- Vanibot/commands/Invite.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    name: 'Invite',
-    description: "Sends invites to our server!",
-    execute(message, args){
-        message.channel.send('https://www.discord.gg/HornyEmpire (Where my mom lives)');
-    }
-}
--- Vanibot/commands/partners.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    name: 'partners',
-    description: "Sends invites to our partners!",
-    execute(message, args){
-        message.channel.send('https://discord.gg/KCx6c5qrDM');
-    }
-}
